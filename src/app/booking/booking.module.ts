430db60facd75c97e89e65634fd1579a8648b6c7
casparsylwan <casparsylwan@gmail.com>
2020-12-20 12:33:00 +0100

    Order completation set up done

--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OrderCompletationComponent } from './order-completation/order-completation.component';
 import { RouterModule } from '@angular/router';
+
+import { OrderCompletationComponent } from './order-completation/order-completation.component';
 import { OrderContainerComponent } from './order-container/order-container.component';
+import { DestinationComponent } from './destination/destination.component';
+import { SeatsComponent } from './seats/seats.component';
+import { PaymentComponent } from './payment/payment.component';
 
 const routes = [
   {
     path:'',
-    component: OrderCompletationComponent
+    component: OrderContainerComponent,
+    children: [
+      {path: 'destination', component: DestinationComponent},
+      {path: 'seats', component: SeatsComponent},
+      {path: 'payment', component: PaymentComponent}
+    ]
   }
 ]
 
 
 @NgModule({
-  declarations: [OrderCompletationComponent, OrderContainerComponent],
+  declarations: [OrderCompletationComponent, OrderContainerComponent, DestinationComponent, SeatsComponent, PaymentComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
